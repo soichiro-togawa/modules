1b10fa3ea3b8d7c6f5665ada3fe4cdea149c750b
soichiro-togawa <soichiro.togawa@gmail.com>
2020-09-20 13:56:10 +0900

    script_push

---
+++ shell_script/git_push.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+cd /mnt/d/"Google ドライブ"/Pipeline
+source /mnt/d/"Google ドライブ"/00Colab Notebooks/00Commands/03SCRIPTkey/github.txt
+git add -A
+git commit -m "script_push"
+
+expect -c "
+set timeout 5
+spawn git push origin master
+expect \"Username\"
+send \"${UN}\n\"
+expect \"Password\"
+send \"${PW}\n\"
+expect \"\\\$\"
+exit 0
+"
+
+#使い方
+# /mnt/d/Google ドライブ/Pipeline/shell_script/git_push.sh
