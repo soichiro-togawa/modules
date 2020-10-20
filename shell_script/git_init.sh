ca825800c3a69e0f6497d10687be418dcb1b535e
soichiro-togawa <soichiro.togawa@gmail.com>
2020-10-20 23:02:34 +0900

    script_push

---
+++ shell_script/git_init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+source "/mnt/d/Google ドライブ/00Colab Notebooks/00Commands/03SCRIPTkey/github.txt"
+repoURL="https://github.com/soichiro-togawa/Git_private.git"
+#コマンドライン引数で上書き可能
+if [ $1 ]; then
+  repoURL=$1
+fi
+
+git init
+git config --global user.name $UN
+git config --global user.email $Email
+git remote add origin $repoURL
+git remote -v
