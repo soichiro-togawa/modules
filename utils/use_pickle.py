40994186de0478c29c87ebe6d7ff9910ab4a3045
soichiro-togawa <soichiro.togawa@gmail.com>
2021-02-17 18:48:19 +0900

    script_push

--- utils/use_pickle.py
+++ utils/use_pickle.py
@@ -1,10 +1,10 @@
 import pickle
 
 def save_pkl(path, data):
-# with open("/content/json.pkl", mode='wb') as f:
-with open(path, mode='wb') as f:
-    pickle.dump(data,f)
+    # with open("/content/json.pkl", mode='wb') as f:
+    with open(path, mode='wb') as f:
+        pickle.dump(data,f)
 
-def load_pkl(path)
-with open(path, mode='rb') as f:
-    return pickle.load(f)
+def load_pkl(path):
+    with open(path, mode='rb') as f:
+        return pickle.load(f)
